959934dd41de46b1137e6dd41db0a59657d0cb20
Polkovnik-Daniil <0205danik@mail.ru>
2021-01-03 07:46:47 +0300

    final version

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,362 @@
+﻿using System;
+namespace Class1
+{
+    public class Airline
+    {
+        public Airline()
+        {
+            Console.WriteLine("");
+        }
+        public static void Show_that_it_ready(string str)
+        {
+            Console.WriteLine("You are read this text from file and this methods take parameter: " + str);
+        }
+        public string Destination;
+        public string Type_Plane;
+        public string flight_number;
+        public string minute;
+        public string hour;
+        public string seconds;
+        public string day;
+        public string month;
+        public string years;
+        public string weekDay;
+        public string[] weekDays = new string[] { "Mon", "Tues", "Wed", "Thut", "Frid", "Sat", "Sund" };
+        public int[] Day_month = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        DateTime dt1 = new DateTime();
+        public string Destination_
+        {
+            get
+            {
+                return Destination;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    Destination = value;
+            }
+        }
+        ///////////////////////////
+        public string Type_Plane_
+        {
+            get
+            {
+                return Type_Plane;
+            }
+            set
+            {
+                if (value == "" && fail == 0)
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    Type_Plane = value;
+            }
+        }
+        ///////////////////////////
+        public string flight_number_
+        {
+            get
+            {
+                return flight_number;
+            }
+            set
+            {
+                if (int.Parse(value) >= Int32.MaxValue)
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+                else
+                    flight_number = value;
+            }
+        }
+        ///////////////////////////
+        public string minute_
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (60 > Convert.ToInt32(value) && fail == 0)
+                {
+                    minute = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string hour_
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                if (24 > Convert.ToInt32(value))
+                {
+                    hour = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        public string day_
+        {
+            get
+            {
+                return day;
+            }
+            set
+            {
+                int month = int.Parse(this.month);
+                if (int.Parse(value) < 32 && Day_month[month - 1] > int.Parse(value))
+                {
+                    day = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string month_
+        {
+            get
+            {
+                return month;
+            }
+            set
+            {
+                if (int.Parse(value) < 13)
+                {
+                    month = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        ///////////////////////////
+        public string years_
+        {
+            get
+            {
+                return years;
+            }
+            set
+            {
+                int year = Convert.ToInt32(time_Now.ToString("yyyy"));
+                if (year > Convert.ToInt32(value))
+                {
+                    years = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+        // not fact
+        ///////////////////////////
+        ///////////////////////////
+        public string weekDay_
+        {
+            get
+            {
+                return weekDay;
+            }
+            set
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    if (value != weekDays[i] && fail == 0)
+                    {
+                        Console.WriteLine("Error cant be written");
+                        fail++;
+                    }
+                    else
+                    {
+                        weekDay = value;
+                        break;
+                    }
+                }
+            }
+        }
+        ///////////////////////////
+        public int fail = 0;
+        public DateTime time_Now = DateTime.Now;
+        ///////////////////////////
+        public string seconds_
+        {
+            get
+            {
+                return seconds;
+            }
+            set
+            {
+                if (int.Parse(value) < 61)
+                {
+                    seconds = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error cant be written");
+                    fail++;
+                }
+            }
+        }
+    }
+    ///////////////////////////
+    class Class
+    {
+        static void day(string string_1, Airline[] array_airline, int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (array_airline[i].weekDay == string_1)
+                {
+                    Console.WriteLine("Destination:" + array_airline[i].Destination);
+                    Console.WriteLine("Type plane: " + array_airline[i].Type_Plane);
+                    Console.WriteLine("flight number" + array_airline[i].flight_number);
+                    Console.WriteLine("Week day" + array_airline[i].weekDay);
+                    Console.WriteLine("Plane arrive:" + array_airline[i].years + "." + array_airline[i].month + "." + array_airline[i].day + " time: " + array_airline[i].hour + ":" + array_airline[i].minute + ":" + array_airline[i].seconds);
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void day_week(string string_, Airline[] array_airline, int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (array_airline[i].Destination == string_)
+                {
+                    Console.WriteLine("Destination:" + array_airline[i].Destination);
+                    Console.WriteLine("Type plane: " + array_airline[i].Type_Plane);
+                    Console.WriteLine("flight number" + array_airline[i].flight_number);
+                    Console.WriteLine("Week day" + array_airline[i].weekDay);
+                    Console.WriteLine("data time: " + array_airline[i].time_Now);
+                    Console.WriteLine("Plane arrive:" + array_airline[i].years + "." + array_airline[i].month + "." + array_airline[i].day + " time: " + array_airline[i].hour + ":" + array_airline[i].minute + ":" + array_airline[i].seconds);
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void Cout(Airline[] array_airline, int j)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if (array_airline[i].fail == 0)
+                {
+                    Console.WriteLine("Destination:" + array_airline[i].Destination);
+                    Console.WriteLine("Type plane: " + array_airline[i].Type_Plane);
+                    Console.WriteLine("flight number" + array_airline[i].flight_number);
+                    Console.WriteLine("Week day" + array_airline[i].weekDay);
+                    Console.WriteLine("data time now: " + array_airline[i].time_Now);
+                    Console.WriteLine("Plane arrive:" + array_airline[i].years + "." + array_airline[i].month + "." + array_airline[i].day + " time: " + array_airline[i].hour + ":" + array_airline[i].minute + ":" + array_airline[i].seconds);
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void add(Airline[] array_airline, int i, int j)
+        {
+            if (j == i - 1)
+            {
+                Array.Resize(ref array_airline, i + 1);
+                i++;
+            }
+            Console.Write("Enter destination: ");
+            array_airline[j].Destination_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter flight number: ");
+            array_airline[j].flight_number_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter type plane: ");
+            array_airline[j].Type_Plane_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter minute arrive: ");
+            array_airline[j].minute_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter hour arrive: ");
+            array_airline[j].hour_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter month arrive: ");
+            array_airline[j].month_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter day arrive: ");
+            array_airline[j].day_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+
+            Console.Write("Enter years arrive: ");
+            array_airline[j].years_ = Console.ReadLine();
+
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                return;
+            }
+            Console.Write("Enter week day: ");
+            array_airline[j].weekDay_ = Console.ReadLine();
+            if (array_airline[j].fail > 0)
+            {
+                Console.WriteLine("Error written");
+                array_airline[j].fail = 0;
+            }
+            else
+            {
+                array_airline[j].fail = 0;
+                j += 1;
+            }
+        }
+        //////////////////////////////////////////
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,272 @@
+﻿using Class1;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    static class Reflection
+    {
+        ///////////////////it`s for example///////////////////
+        public static string str { get; set; }
+        public static int integ { get; set; }
+        public static int a;
+        public static string a_;
+        public static string Display_information_Full_Name(Type t)
+        {
+            string a = t.FullName;
+            return a;
+        }
+        public static ConstructorInfo[] Display_Type()
+        {
+            object obj = Display_information_Full_Name(typeof(Reflection));
+            Type type = obj.GetType();
+            ConstructorInfo[] info = type.GetConstructors();
+            return info;
+        }
+        public static MethodInfo[] Getmethod(Type t)
+        {
+            MethodInfo[] MArr = t.GetMethods(BindingFlags.Public | BindingFlags.Static);
+            return MArr;
+        }
+        public static PropertyInfo[] Get_Properties(Type t)
+        {
+            PropertyInfo[] myPropertyInfo = t.GetProperties();
+            return myPropertyInfo;
+        }
+        public static FieldInfo[] Get_Field(Type t)
+        {
+            FieldInfo[] fieldNames = t.GetFields();
+            return fieldNames;
+        }
+        public static object[] Get_Interface(Type t)
+        {
+            object[] intef = t.GetInterfaces();
+            return intef;
+        }
+        public static string WriteToFile(string fullname, ConstructorInfo[] info, MethodInfo[] arrMethodInfo, PropertyInfo[] myPropertyInfo, FieldInfo[] myFieldInfo, object[] inter)
+        {
+            string writePath = @"D:\Даник\Учеба\3 семестр\ООП\Лабораторные работы\12\ConsoleApp1\ConsoleApp1\file.txt";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("Full Name: \t\n\t\t" + fullname);
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("Constructors: ");
+                    for (int i = 0; i < info.Length; i++)
+                        sw.WriteLine("\t\t" + info[i]);
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("Methods: ");
+                    for (int i = 0; i < arrMethodInfo.Length; i++)
+                        sw.WriteLine("\t\t" + arrMethodInfo[i]);
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////            
+                    sw.WriteLine("Properties: ");
+                    foreach (var t in myPropertyInfo)
+                        sw.WriteLine("\t\t" + t);
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("Fields: ");
+                    foreach (var t in myFieldInfo)
+                        sw.WriteLine("\t\t" + t);
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("---------------------------------------");
+                    sw.WriteLine("Interfaces: ");
+                    foreach (var t in inter)
+                        sw.WriteLine("\t\t" + t);
+                    sw.WriteLine("---------------------------------------");
+                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    sw.WriteLine("Parameters Methods: ");
+                    for (int i = 0; i < arrMethodInfo.Length; i++)
+                    {
+                        sw.WriteLine("\tMethods:\t" + arrMethodInfo[i]);
+                        sw.Write("\t Parameters:\t ");
+                        ParameterInfo[] methodInfoParamet = arrMethodInfo[i].GetParameters();
+                        foreach (var it in methodInfoParamet)
+                        {
+                            sw.WriteLine("" + it);
+                            sw.Write("\t\t\t ");
+                        }
+                        sw.WriteLine();
+                    }
+                    sw.WriteLine("---------------------------------------");
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return writePath;
+        }
+        public static bool ReadToFile(string path, string name_method, string Name_Class)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    bool bl = false;
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = line.Remove(0, 2);
+                        if (name_method == line)
+                        {
+                            bl = true;
+                        }
+                        if (Name_Class == line && bl == false)
+                        {
+                            sr.Close();
+                            return true;
+                        }
+                        else if (bl == true)
+                        {
+                            return false;
+                        }
+                    }
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return false;
+
+        }
+        public static void Call_method(string name_method, string Name_Class, string str)
+        {
+            try
+            {
+                Type type = Type.GetType(Name_Class);
+                if (type != null)
+                {
+                    MethodInfo method = typeof(Airline).GetMethod(name_method);                                                                                                             //Void Show_that_it_ready(System.String)
+                    if (method != null)
+                    {
+                        object result = null;
+                        ParameterInfo[] parameters = method.GetParameters();
+                        object p = Activator.CreateInstance(type, null);
+                        if (parameters.Length == 0)
+                        {
+                            result = method.Invoke(p, null);
+                            Console.WriteLine(result);
+                        }
+                        else
+                        {
+                            object[] parametersArray = new object[] { str };
+                            result = method.Invoke(p, parametersArray);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static Type Create(string type)
+        {
+            Type tp = Type.GetType(type);
+            return tp;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Type t1 = typeof(Reflection).GetType();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Полное Имя:             {0}", t1.FullName);
+            Console.WriteLine("Базовый класс:          {0}", t1.BaseType);
+            Console.WriteLine("Абстрактный:            {0}", t1.IsAbstract);
+            Console.WriteLine("Это COM объект:         {0}", t1.IsCOMObject);
+            Console.WriteLine("Запрещено наследование: {0}", t1.IsSealed);
+            Console.WriteLine("Это class:              {0}", t1.IsClass);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string fullname = Reflection.Display_information_Full_Name(typeof(Reflection));
+            Console.WriteLine("Full Name: \t\n\t\t" + fullname);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            ConstructorInfo[] info = Reflection.Display_Type();
+            Console.WriteLine("Constructors: ");
+            for (int i = 0; i < info.Length; i++)
+                Console.WriteLine("\t\t" + info[i]);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            MethodInfo[] arrMethodInfo = Reflection.Getmethod(typeof(Airline));
+            Console.WriteLine("Methods: ");
+            for (int i = 0; i < arrMethodInfo.Length; i++)
+                Console.WriteLine("\t\t" + arrMethodInfo[i]);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////            
+            PropertyInfo[] myPropertyInfo = Reflection.Get_Properties(typeof(Reflection));
+            Console.WriteLine("Properties: ");
+            foreach (var t in myPropertyInfo)
+                Console.WriteLine("\t\t" + t);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            FieldInfo[] myFieldInfo = Reflection.Get_Field(typeof(Reflection));
+            Console.WriteLine("Fields: ");
+            foreach (var t in myFieldInfo)
+                Console.WriteLine("\t\t" + t);
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("---------------------------------------");
+            object[] inter = Reflection.Get_Interface(typeof(Reflection));
+            Console.WriteLine("Interfaces: ");
+            foreach (var t in inter)
+                Console.WriteLine("\t\t" + t);
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Parameters Methods: ");
+            for (int i = 0; i < arrMethodInfo.Length; i++)
+            {
+                Console.WriteLine("\tMethods:\t" + arrMethodInfo[i]);
+                Console.Write("\t Parameters:\t ");
+                ParameterInfo[] methodInfoParamet = arrMethodInfo[i].GetParameters();
+                foreach (var it in methodInfoParamet)
+                {
+                    Console.WriteLine("" + it);
+                    Console.Write("\t\t\t ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------------------");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string Path = Reflection.WriteToFile(fullname, info, arrMethodInfo, myPropertyInfo, myFieldInfo, inter);
+            string Name_Class;
+            Name_Class = "ConsoleApp1.Reflection";
+            Console.WriteLine("Enter name of method: ");
+            string name_method = Console.ReadLine();
+            name_method = "Void Show_that_it_ready(System.String)";
+            bool bl = Reflection.ReadToFile(Path, name_method, Name_Class);
+            if (bl == true)
+            {
+                Name_Class = "Class1.Airline";
+                Console.WriteLine("Method was found!");
+                Console.Write("Enter string for confirmation! ");
+                string str = Console.ReadLine();
+                name_method = "Show_that_it_ready";
+                Reflection.Call_method(name_method, Name_Class, str);
+            }
+            else
+            {
+                Console.WriteLine("Method wasn`t found! Because was enter uncorrected value");
+            }
+            string str_ = "ConsoleApp1.Reflection"; 
+            Type tp = Reflection.Create(str_);
+            Console.WriteLine("was create object with type: \t" + tp);
+            Console.WriteLine("Bye World");
+            Console.ReadKey();
+        }
+    }
+}
